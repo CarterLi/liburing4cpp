5be321018bce0e6150a3f677eb3e745796d357d6
李通洲 <carter.li@eoitek.com>
2020-09-25 10:38:36 +0800

    echo_server.cpp: Removes USE_FIXED_FILES_AND_BUFFERS

--- demo/echo_server.cpp
+++ demo/echo_server.cpp
@@ -8,7 +8,6 @@
 
 #include "io_service.hpp"
 
-#define USE_FIXED_FILES_AND_BUFFERS 0
 #define USE_LINK 0
 #define USE_POLL 0
 
@@ -19,76 +18,25 @@ enum {
 
 int runningCoroutines = 0;
 
-#if USE_FIXED_FILES_AND_BUFFERS
-std::array<iovec, MAX_CONN_SIZE> fixedBuffers;
-#endif
-
 task<> accept_connection(io_service& service, int serverfd) {
-#if USE_FIXED_FILES_AND_BUFFERS
-    std::vector<int> availKeys(MAX_CONN_SIZE);
-    std::iota(availKeys.rbegin(), availKeys.rend(), 0);
-#endif
-
     while (int clientfd = co_await service.accept(serverfd, nullptr, nullptr)) {
-        [=, &service
-#if USE_FIXED_FILES_AND_BUFFERS
-        , &availKeys
-#endif
-        ](int clientfd) -> task<> {
-#if USE_FIXED_FILES_AND_BUFFERS
-            if (__builtin_expect(!availKeys.empty(), true)) {
-                const int keyIdx = availKeys.back();
-                availKeys.pop_back();
-                void* pbuf = fixedBuffers[keyIdx].iov_base;
+        [=, &service](int clientfd) -> task<> {
+            fmt::print("sockfd {} is accepted; number of running coroutines: {}\n",
+                clientfd, ++runningCoroutines);
 
-                fmt::print("sockfd {} is accepted; use keyidx: {}; number of running coroutines: {}\n",
-                    clientfd, keyIdx, ++runningCoroutines);
-                service.register_files_update(keyIdx, &clientfd, 1);
-
-                while (true) {
-#   if USE_LINK
-#       if USE_POLL
-                    service.poll(keyIdx, POLLIN, IOSQE_FIXED_FILE | IOSQE_IO_LINK);
-#       endif
-                    auto tread = service.read_fixed(keyIdx, pbuf, BUF_SIZE, 0, keyIdx, IOSQE_IO_LINK | IOSQE_FIXED_FILE);
-                    // If a short read is found, write_fixed will be canceled with -ECANCELED
-                    int w = co_await service.write_fixed(keyIdx, pbuf, BUF_SIZE, 0, keyIdx, IOSQE_FIXED_FILE);
-                    if (w < 0) {
-                        int r = tread.get_result();
-                        if (r <= 0) break;
-                        co_await service.write_fixed(keyIdx, pbuf, r, 0, keyIdx, IOSQE_FIXED_FILE);
-                    }
-#   else
-#       if USE_POLL
-                    co_await service.poll(keyIdx, POLLIN, IOSQE_FIXED_FILE);
-#       endif
-                    int r = co_await service.read_fixed(keyIdx, pbuf, BUF_SIZE, 0, keyIdx, IOSQE_FIXED_FILE);
-                    if (r <= 0) break;
-                    co_await service.write_fixed(keyIdx, pbuf, r, 0, keyIdx, IOSQE_FIXED_FILE);
-#   endif
-                }
-                availKeys.push_back(keyIdx);
-            } else {
-#endif
-                fmt::print("sockfd {} is accepted; number of running coroutines: {}\n",
-                    clientfd, ++runningCoroutines);
-
-                std::vector<char> buf(BUF_SIZE);
-                while (true) {
+            std::vector<char> buf(BUF_SIZE);
+            while (true) {
 #if USE_POLL
 #   if USE_LINK
-                    service.poll(clientfd, POLLIN, IOSQE_IO_LINK);
+                service.poll(clientfd, POLLIN, IOSQE_IO_LINK);
 #   else
-                    co_await service.poll(clientfd, POLLIN);
+                co_await service.poll(clientfd, POLLIN);
 #   endif
 #endif
-                    int r = co_await service.recv(clientfd, buf.data(), BUF_SIZE, MSG_NOSIGNAL);
-                    if (r <= 0) break;
-                    co_await service.send(clientfd, buf.data(), r, MSG_NOSIGNAL);
-                }
-#if USE_FIXED_FILES_AND_BUFFERS
+                int r = co_await service.recv(clientfd, buf.data(), BUF_SIZE, MSG_NOSIGNAL);
+                if (r <= 0) break;
+                co_await service.send(clientfd, buf.data(), r, MSG_NOSIGNAL);
             }
-#endif
             shutdown(clientfd, SHUT_RDWR);
             fmt::print("sockfd {} is closed; number of running coroutines: {}\n",
                 clientfd, --runningCoroutines);
@@ -107,18 +55,6 @@ int main(int argc, char *argv[]) {
     }
 
     io_service service(MAX_CONN_SIZE);
-
-#if USE_FIXED_FILES_AND_BUFFERS
-    std::array<int, MAX_CONN_SIZE> fds;
-    std::fill(fds.begin(), fds.end(), -1);
-    service.register_files(fds.data(), fds.size());
-
-    for (auto& iov : fixedBuffers) {
-        iov.iov_base = new char[BUF_SIZE];
-        iov.iov_len = BUF_SIZE;
-    }
-    service.register_buffers(fixedBuffers.data(), fixedBuffers.size());
-#endif
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0) | panic_on_err("socket creation", true);
     on_scope_exit closesock([=]() { shutdown(sockfd, SHUT_RDWR); });
