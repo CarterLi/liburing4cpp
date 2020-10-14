4322730e4a27e71c61655b716356da27c67d2f72
李通洲 <carter.li@eoitek.com>
2020-10-14 15:54:14 +0800

    io_service: print probe info when debugging

--- io_service.hpp
+++ io_service.hpp
@@ -118,6 +118,47 @@ public:
         io_uring_queue_init_params(entries, &ring, &p) | panic_on_err("queue_init_params", false);
 
 #ifndef NDEBUG
+        auto* probe = io_uring_get_probe_ring(&ring);
+        on_scope_exit free_probe([=]() { io_uring_free_probe(probe); });
+#   define TEST_IORING_OP(opcode) do { for (int i = 0; i < probe->ops_len; ++i) { if (probe->ops[i].op == opcode && probe->ops[i].flags & IO_URING_OP_SUPPORTED) { puts("\t" #opcode); break; } } } while (0)
+        puts("Supported io_uring opcodes by current kernel:");
+        TEST_IORING_OP(IORING_OP_NOP);
+        TEST_IORING_OP(IORING_OP_READV);
+        TEST_IORING_OP(IORING_OP_WRITEV);
+        TEST_IORING_OP(IORING_OP_FSYNC);
+        TEST_IORING_OP(IORING_OP_READ_FIXED);
+        TEST_IORING_OP(IORING_OP_WRITE_FIXED);
+        TEST_IORING_OP(IORING_OP_POLL_ADD);
+        TEST_IORING_OP(IORING_OP_POLL_REMOVE);
+        TEST_IORING_OP(IORING_OP_SYNC_FILE_RANGE);
+        TEST_IORING_OP(IORING_OP_SENDMSG);
+        TEST_IORING_OP(IORING_OP_RECVMSG);
+        TEST_IORING_OP(IORING_OP_TIMEOUT);
+        TEST_IORING_OP(IORING_OP_TIMEOUT_REMOVE);
+        TEST_IORING_OP(IORING_OP_ACCEPT);
+        TEST_IORING_OP(IORING_OP_ASYNC_CANCEL);
+        TEST_IORING_OP(IORING_OP_LINK_TIMEOUT);
+        TEST_IORING_OP(IORING_OP_CONNECT);
+        TEST_IORING_OP(IORING_OP_FALLOCATE);
+        TEST_IORING_OP(IORING_OP_OPENAT);
+        TEST_IORING_OP(IORING_OP_CLOSE);
+        TEST_IORING_OP(IORING_OP_FILES_UPDATE);
+        TEST_IORING_OP(IORING_OP_STATX);
+        TEST_IORING_OP(IORING_OP_READ);
+        TEST_IORING_OP(IORING_OP_WRITE);
+        TEST_IORING_OP(IORING_OP_FADVISE);
+        TEST_IORING_OP(IORING_OP_MADVISE);
+        TEST_IORING_OP(IORING_OP_SEND);
+        TEST_IORING_OP(IORING_OP_RECV);
+        TEST_IORING_OP(IORING_OP_OPENAT2);
+        TEST_IORING_OP(IORING_OP_EPOLL_CTL);
+        TEST_IORING_OP(IORING_OP_SPLICE);
+        TEST_IORING_OP(IORING_OP_PROVIDE_BUFFERS);
+        TEST_IORING_OP(IORING_OP_REMOVE_BUFFERS);
+        TEST_IORING_OP(IORING_OP_TEE);
+        TEST_IORING_OP(IORING_OP_SHUTDOWN);
+#   undef TEST_IORING_OP
+
 #   define TEST_IORING_FEATURE(feature) if (p.features & feature) puts("\t" #feature)
         puts("Supported io_uring features by current kernel:");
         TEST_IORING_FEATURE(IORING_FEAT_SINGLE_MMAP);
@@ -127,6 +168,7 @@ public:
         TEST_IORING_FEATURE(IORING_FEAT_CUR_PERSONALITY);
         TEST_IORING_FEATURE(IORING_FEAT_FAST_POLL);
         TEST_IORING_FEATURE(IORING_FEAT_POLL_32BITS);
+        TEST_IORING_FEATURE(IORING_FEAT_SQPOLL_NONFIXED);
 #   undef TEST_IORING_FEATURE
 #endif
     }
@@ -493,7 +535,6 @@ public:
         io_uring_prep_statx(sqe, dfd, path, flags, mask, statxbuf);
         return await_work(sqe, iflags);
     }
-
 
     /** Splice data to/from a pipe asynchronously
      * @see splice(2)
