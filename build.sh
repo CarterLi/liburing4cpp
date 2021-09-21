fd6db5c7d6e9261129a291a6f5c241d7344c580f
Alecto Irene Perez <perez.cs@pm.me>
2021-09-21 15:11:05 -0600

    Added build script build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Move to the root directory of the project
+# build.sh should be in the project root 
+cd "$(dirname "$0")"
+
+
+# Build things in a build directory named 'build' in the project root 
+# Output executables in a directory named 'bin' in the project root 
+cmake . -B build -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$PWD/bin"
+cmake --build build
