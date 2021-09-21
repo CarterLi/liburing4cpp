9f781fa275b971793dd76b7b91b38edc6506d371
Alecto Irene Perez <perez.cs@pm.me>
2021-09-21 17:18:22 -0600

    Improved documenting/layout of CMakeLists.txt

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Move to the root directory of the project
-# build.sh should be in the project root 
+# build.sh should be in the project root
 cd "$(dirname "$0")"
 
-
-# Build things in a build directory named 'build' in the project root 
-# Output executables in a directory named 'bin' in the project root 
-cmake . -B build -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$PWD/bin"
+# Build things in a build directory named 'build' in the project root
+# Output executables in a directory named 'bin' in the project root
+cmake . -B build
 cmake --build build
