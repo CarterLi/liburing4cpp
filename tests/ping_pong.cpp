8e2ed4725f9ce80de0f7ed608987037838135586
Alecto Irene Perez <perez.cs@pm.me>
2021-09-25 21:48:06 -0600

    Updated tests/ping_pong.cpp
    Fixed error handling (errors are returned as -errno from function. Also
    updated formatting to match project formatting guidelines.
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- tests/ping_pong.cpp
+++ tests/ping_pong.cpp
@@ -3,14 +3,16 @@
 
 #include <liburing/io_service.hpp>
 #include <string_view>
+
 auto ping(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
-    std::string_view ping = "ping!";
+    std::string_view msg = "ping!";
+    std::string_view expected = "pong!";
     std::array<char, 64> buffer;
+
     for (int i = 0; i < 20; i++) {
         int count =
-            co_await service.read(read_fd, buffer.data(), buffer.size(), 0);
-        if (count < 0)
-            uio::panic("Bad read", errno);
+            co_await service.read(read_fd, buffer.data(), buffer.size(), 0)
+            | uio::panic_on_err("ping: Unable to read from read_fd", false);
 
         auto recieved = std::string_view(buffer.data(), count);
 
@@ -18,26 +20,37 @@ auto ping(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
             fg(fmt::color::cyan) | fmt::emphasis::bold,
             "ping: Recieved {}\n",
             recieved);
-        if (recieved != "pong!")
+        if (recieved != expected)
             uio::panic("Unexpected message", 0);
 
-        co_await service.write(write_fd, ping.data(), ping.size(), 0);
-        co_await service.fsync(write_fd, 0);
+        co_await service.write(write_fd, msg.data(), msg.size(), 0)
+            | uio::panic_on_err("ping: Unable to write to write_fd", false);
     }
-    close(read_fd);
-    close(write_fd);
+
+    co_await service.close(write_fd)
+        | uio::panic_on_err("ping: Unable to close write_fd", false);
+
+    // Check for EOF before exiting
+    if(0 != co_await service.read(read_fd, buffer.data(), buffer.size(), 0)) {
+        throw std::runtime_error("pong: Pipe not at EOF like expected");
+    }
+
+    co_await service.close(read_fd)
+        | uio::panic_on_err("ping: Unable to close read_fd", false);
 }
+
 auto pong(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
-    std::string_view ping = "pong!";
+    std::string_view msg = "pong!";
+    std::string_view expected = "ping!";
     std::array<char, 64> buffer;
+
     for (int i = 0; i < 20; i++) {
-        co_await service.write(write_fd, ping.data(), ping.size(), 0);
-        co_await service.fsync(write_fd, 0);
+        co_await service.write(write_fd, msg.data(), msg.size(), 0)
+            | uio::panic_on_err("pong: Unable to write to write_fd", false);
 
         int count =
-            co_await service.read(read_fd, buffer.data(), buffer.size(), 0);
-        if (count < 0)
-            uio::panic("Bad read", errno);
+            co_await service.read(read_fd, buffer.data(), buffer.size(), 0)
+            | uio::panic_on_err("pong: Unable to read from read_fd", false);
 
         auto recieved = std::string_view(buffer.data(), count);
 
@@ -45,12 +58,22 @@ auto pong(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
             fg(fmt::color::magenta) | fmt::emphasis::bold,
             "pong: Recieved {}\n",
             recieved);
-        if (recieved != "ping!")
+        if (recieved != expected)
             uio::panic("Unexpected message", 0);
     }
-    close(read_fd);
-    close(write_fd);
+
+    co_await service.close(write_fd)
+        | uio::panic_on_err("pong: Unable to close write_fd", false);
+
+    // Check for EOF before exiting
+    if(0 != co_await service.read(read_fd, buffer.data(), buffer.size(), 0)) {
+        throw std::runtime_error("pong: Pipe not at EOF like expected");
+    }
+
+    co_await service.close(read_fd)
+        | uio::panic_on_err("pong: Unable to close read_fd", false);
 }
+
 int main() {
     using uio::io_service;
     using uio::task;
