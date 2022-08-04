d5c34edad1935b4d6efa705e7b856765e10e09fe
李通洲 <carter.li@eoitek.com>
2022-08-04 22:50:46 +0800

    Fix segfault

--- io_uring/bench.cpp
+++ io_uring/bench.cpp
@@ -18,41 +18,42 @@ struct stopwatch {
 
 int main(int argc, char *argv[]) noexcept {
     io_host host(24);
-    new io_coroutine(host, [](io_coroutine& coro) {
-        const auto iteration = 10000000;
+    const auto iteration = 10000000;
 
+    new io_coroutine(host, [](io_coroutine& coro) {
         {
             stopwatch sw("coro.nop().await()");
             for (int i = 0; i < iteration; ++i) {
                 coro.nop().await();
             }
         }
-        {
-            stopwatch sw("plain IORING_OP_NOP:");
-            for (int i = 0; i < iteration; ++i) {
-                auto* ring = &coro.host.ring;
-                auto* sqe = io_uring_get_sqe(ring);
-                io_uring_prep_nop(sqe);
-                io_uring_submit_and_wait(ring, 1);
-
-                io_uring_cqe *cqe;
-                io_uring_peek_cqe(ring, &cqe);
-                (void) cqe->res;
-                io_uring_cqe_seen(ring, cqe);
-            }
-        }
-        {
-            stopwatch sw("this_thread::yield:");
-            for (int i = 0; i < iteration; ++i) {
-                std::this_thread::yield();
-            }
-        }
-        {
-            stopwatch sw("pause:");
-            for (int i = 0; i < iteration; ++i) {
-                __builtin_ia32_pause();
-            }
-        }
     });
     host.run();
+
+    {
+        stopwatch sw("plain IORING_OP_NOP:");
+        for (int i = 0; i < iteration; ++i) {
+            auto* ring = &host.ring;
+            auto* sqe = io_uring_get_sqe(ring);
+            io_uring_prep_nop(sqe);
+            io_uring_submit_and_wait(ring, 1);
+
+            io_uring_cqe *cqe;
+            io_uring_peek_cqe(ring, &cqe);
+            (void) cqe->res;
+            io_uring_cqe_seen(ring, cqe);
+        }
+    }
+    {
+        stopwatch sw("this_thread::yield:");
+        for (int i = 0; i < iteration; ++i) {
+            std::this_thread::yield();
+        }
+    }
+    {
+        stopwatch sw("pause:");
+        for (int i = 0; i < iteration; ++i) {
+            __builtin_ia32_pause();
+        }
+    }
 }
